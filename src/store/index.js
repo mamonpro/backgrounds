2229c3942140363f9b5ea77843c5569d94d04a03
Sergey Loysha <sloysha@darwinapps.com>
2018-02-18 18:24:16 +0300

    upd

--- src/store/index.js
+++ src/store/index.js
@@ -7,93 +7,123 @@ export default new Vuex.Store({
   state: {
     images: [
       {
+        id: 1,
         url: '1.jpeg'
       },
       {
+        id: 2,
         url: '2.jpeg'
       },
       {
+        id: 3,
         url: '3.jpeg'
       },
       {
+        id: 4,
         url: '4.jpeg'
       },
       {
+        id: 5,
         url: '5.jpeg'
       },
       {
+        id: 6,
         url: '6.jpeg'
       },
       {
+        id: 7,
         url: '7.jpeg'
       },
       {
+        id: 8,
         url: '8.jpeg'
       },
       {
+        id: 9,
         url: '9.jpeg'
       },
       {
+        id: 10,
         url: '10.jpeg'
       },
       {
+        id: 11,
         url: '11.jpeg'
       },
       {
+        id: 12,
         url: '12.jpeg'
       },
       {
+        id: 13,
         url: '13.jpeg'
       },
       {
+        id: 14,
         url: '14.jpeg'
       },
       {
+        id: 15,
         url: '15.jpeg'
       },
       {
+        id: 16,
         url: '16.jpeg'
       },
       {
+        id: 17,
         url: '17.jpeg'
       },
       {
+        id: 18,
         url: '18.jpeg'
       },
       {
+        id: 19,
         url: '19.jpeg'
       },
       {
+        id: 20,
         url: '20.jpeg'
       },
       {
+        id: 21,
         url: '21.jpeg'
       },
       {
+        id: 22,
         url: '22.jpeg'
       },
       {
+        id: 23,
         url: '23.jpeg'
       },
       {
+        id: 24,
         url: '24.jpeg'
       },
       {
+        id: 25,
         url: '25.jpeg'
       },
       {
+        id: 26,
         url: '26.jpeg'
       },
       {
+        id: 27,
         url: '27.jpeg'
       },
       {
+        id: 28,
         url: '28.jpeg'
       },
       {
+        id: 29,
         url: '29.jpeg'
       },
       {
+        id: 30,
         url: '30.jpeg'
       }
     ]
